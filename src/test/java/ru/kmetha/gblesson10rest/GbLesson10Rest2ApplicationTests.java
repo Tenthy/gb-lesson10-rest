53e5236fb3468982e6a70593431bb8594bb4ac12
Kmetha <rakursdead@gmail.com>
2022-05-03 15:07:18 +0300

    Initial commit

---
+++ src/test/java/ru/kmetha/gblesson10rest/GbLesson10Rest2ApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.kmetha.gblesson10rest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GbLesson10Rest2ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
